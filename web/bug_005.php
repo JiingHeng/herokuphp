c36ade9f74429dd2450b3b668b6a869e55831fa2
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 23:02:53 +0800

    commit mesage goes here

--- web/bug_005.php
+++ web/bug_005.php
@@ -9,6 +9,6 @@
 </head>
 <body>
     <!--Error 5: Lists do not contain only <li> elements and script supporting elements (<script> and <template>)-->
-    <li> <p> List item 1 </p> </li>
+    <ul> <p> List item 1 </p> </ul>
 </body>
 </html>
