b3fe70aa692448c9b258b4555ee4ff27b16c1201
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 13:01:51 +0800

    commit mesage goes here

---
+++ web/errorForExercise3.php
@@ -0,0 +1,16 @@
+<!--Fix: Page lacks the HTML doctype, thus triggering quirks mode-->
+<!-- <!DOCTYPE html> -->
+<!-- Fix : <html> element does not have a [lang] attribute -->
+<!-- <html lang="en"> -->
+<head>
+    <!--Fix: Does not have a <meta name=”viewport”> tag with width or initial-scale -->
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <!--Fix: Document does not have a meta description-->
+    <!-- <meta name="description" content="RP worksheet exerise for Lesson 08"> -->
+</head>
+<body>
+    <!--Fix: Document doesn't have a <title> element -->
+    <!-- <title>Software Testing worksheet</title> -->
+    Hello World
+</body>
+</html>
