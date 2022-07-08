efaca9cfce66448e15b49f4d14bdc1f78d7205a8
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 16:12:21 +0800

    commit mesage goes here

---
+++ web/bug_006.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--ERROR 4: <html> element does not have a [lang] attribute-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="RP worksheet exerise 4 for Lesson 08">
+    <title>Document</title>
+</head>
+<body>
+    <!--Error 6: Heading Elements are not in a sequentially-descending order-->
+    <h3> Page title </h3>
+</body>
+</html>
---
+++ web/bug_006_fixed.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="RP worksheet exerise 4 for Lesson 08">
+    <title>Document</title>
+</head>
+<body>
+    <!--Fix 6: Heading Elements are not in a sequentially-descending order-->
+    <h1> Page title </h1>
+    <h3> Section 1 of page </h3>
+    <h3> Section 2 of page </h3>
+</body>
+</html>
