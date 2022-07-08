d031b3ad0d121c62fb470b25fef67e4116099d11
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 15:18:07 +0800

    commit mesage goes here

--- web/Bug 1/bug_001_fixed.php
+++ web/Bug 1/bug_001_fixed.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="RP worksheet exerise 4 for Lesson 08">
     <title>Document</title>
 </head>
 <body>
-    <!--Fix 2: Optimize Cumulative Layout Shift-->
+    <!--Fix 1: Image element does not have explicit width and height-->
     <img src="web/resources/img/wallpaper.webp" width="1536" height="1024" alt="wallpaper">
 </body>
 </html>
