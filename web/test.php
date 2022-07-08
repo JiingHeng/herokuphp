3fc9b8a251f902c02fbc7ac8d19fcc5efcfb573e
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 12:13:08 +0800

    commit mesage goes here

--- web/test.php
+++ web/test.php
@@ -1,8 +1,10 @@
 <html lang="en">
 <head>
+    <!-- Fix Does not have a <meta name=”viewport”> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+    <title>Software Testing worksheet</title>
     Hello World
 </body>
 </html>
