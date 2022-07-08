9afb477608874a9d3e8bc5345b3067be19d45a67
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 13:51:22 +0800

    commit mesage goes here

---
+++ web/NYP ERRORS/nypErrors.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--Error 1: Serve responsive images-->
+    <img src="web/resources/img/wallpaper.jpg">
+    <!--Error 2: Optimize Cumulative Layout Shift-->
+    <!-- <img src="web/resources/img/wallpaper.jpg"> -->
+</body>
+</html>
---
+++ web/NYP ERRORS/nypErrorsFixed.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--Fix 1: Serve responsive images-->
+    <img src="web/resources/img/wallpaper.webp" srcset="wallpaper.webp 1536w" sizes="50vw">
+    <!--Fix 2: Optimize Cumulative Layout Shift-->
+    <!-- <img src="web/resources/img/wallpaper.webp" width="1536" height="1024" alt="wallpaper"> -->
+
+</body>
+</html>
