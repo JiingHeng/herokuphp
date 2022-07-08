2bf96dff9764a698e55d517b152b67e10d524d44
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 22:52:59 +0800

    commit mesage goes here

---
+++ web/bug_009_fixed.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="RP worksheet exerise 4 for Lesson 08">
+    <title>Document</title>
+</head>
+<body>
+    <!--Fix 9: Document doesn't use legible font sizes-->
+    <h1 style="font-size: 13px">Hello</h1></body>
+</html>
