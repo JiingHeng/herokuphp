5042401e15fd5ab5d2fec2fb8c5c15d110faf284
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 21:49:48 +0800

    commit mesage goes here

---
+++ web/bug_008.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="RP worksheet exerise 4 for Lesson 08">
+    <title>Document</title>
+</head>
+<body>
+    <!--Error 8: Links do not have descriptive text-->
+    <p>Click here, <a href="bug_008_fixed.php"></a></p>
+</body>
+</html>
---
+++ web/bug_008_fixed.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="RP worksheet exerise 4 for Lesson 08">
+    <title>Document</title>
+</head>
+<body>
+    <!--Fix 8: Links do not have descriptive text-->
+    <p>To view the bug/error please <a href="bug_008.php">Click here!!</a></p>
+</body>
+</html>
