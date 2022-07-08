2a115f06316901e5ad4c734bb76c591046bf4266
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 16:30:06 +0800

    commit mesage goes here

---
+++ web/bug_007_fixed.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="RP worksheet exerise 4 for Lesson 08">
+    <title>Document</title>
+</head>
+<body>
+    <!--Fix 7: Links do not have descriptive text-->
+    <p> To , <a href="web/NYP ERRORS/nypErrors.php"</a> for the error page.</p>
+</body>
+</html>
