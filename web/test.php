c6f7283f0ad8d129627dc8657b7061faf1daa7b6
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 12:26:58 +0800

    commit mesage goes here

--- web/test.php
+++ web/test.php
@@ -1,9 +1,12 @@
+<!--Fix: Page lacks the HTML doctype, thus triggering quirks mode-->
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Fix Does not have a <meta name=”viewport”> tag with width or initial-scale -->
+    <!--Fix: Does not have a <meta name=”viewport”> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+    <!--Fix: Document doesn't have a <title> element -->
     <title>Software Testing worksheet</title>
     Hello World
 </body>
