4851361203c2963ef77c62dbe44636ed68985d2a
JiingHeng <20013327@myrp.edu.sg>
2022-07-08 16:17:00 +0800

    commit mesage goes here

--- web/bug_006.php
+++ web/bug_006.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<!--ERROR 4: <html> element does not have a [lang] attribute-->
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
